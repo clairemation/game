25b5318d1a54793ba6a7b3f2329fe2000ab4b175
Claire Samuels <clairemation@gmail.com>
2018-05-11 09:13:17 -0400

    Clean up

--- src/lib/intersection.js
+++ src/lib/intersection.js
@@ -1,6 +1,4 @@
 // https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect/1968345#1968345
-// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines
-// intersect the intersection point may be stored in the floats i_x and i_y.
 function intersection(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y)
 {
     var s1_x, s1_y, s2_x, s2_y;
@@ -13,11 +11,6 @@ function intersection(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y)
 
     if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
     {
-        // Collision detected
-        // if (i_x != NULL)
-        //     *i_x = p0_x + (t * s1_x);
-        // if (i_y != NULL)
-        //     *i_y = p0_y + (t * s1_y);
         return [p0_x + (t * s1_x), p0_y + (t * s1_y)];
     }
 
